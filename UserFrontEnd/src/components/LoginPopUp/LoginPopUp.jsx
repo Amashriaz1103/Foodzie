59b5ecc45beb833709131b8e3c707adb64e82d96
Amash Riaz <129083409+Amashriaz1103@users.noreply.github.com>
2025-03-17 04:25:20 +0530

    cart page updated

--- UserFrontEnd/src/components/LoginPopUp/LoginPopUp.jsx
+++ UserFrontEnd/src/components/LoginPopUp/LoginPopUp.jsx
@@ -12,6 +12,28 @@ const LoginPopUp = ({setShowLogin}) => {
                 <h2>{curState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
+            <div className='login-popup-inputs'>
+              {curState!=="Log In" && <input type="text" placeholder='Your Name' required/>}
+              <input type="email" placeholder='Your Email' required/>
+              <input type="password" placeholder='Password' required/>
+              <button className='btn'>{curState==="Sign Up"?"Create Account":"Log in"}</button>
+            </div>
+            <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>{curState === "Sign Up" ? "By continuing I agree to the terms of privacy & policy" : "Remember Me"}</p>
+        </div>
+
+        <p className="toggle-form">
+          {curState === "Sign Up" ? (
+            <>
+              Already have an account? <span onClick={() => setCurState("Log In")}>Log In</span>
+            </>
+          ) : (
+            <>
+              Don't have an account? <span onClick={() => setCurState("Sign Up")}>Sign Up</span>
+            </>
+          )}
+        </p>
         </form>
     </div>
   )

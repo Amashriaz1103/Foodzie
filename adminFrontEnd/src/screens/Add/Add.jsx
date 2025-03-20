253bf33a675755a185ea0ec312a349c081b5ed15
Amash Riaz <129083409+Amashriaz1103@users.noreply.github.com>
2025-03-20 20:31:03 +0530

    Add order section made

--- adminFrontEnd/src/screens/Add/Add.jsx
+++ adminFrontEnd/src/screens/Add/Add.jsx
@@ -2,8 +2,46 @@ import React from 'react'
 import './Add.css'
 const Add = () => {
   return (
-    <div>
-      Add
+    <div className='screen'>
+      <div className='container'>
+        <form>
+          <div className='add-img-upload'>
+            <p>Upload Image</p>
+            <label htmlFor="image">
+              <img src="{assets.upload_area}" alt="" />
+            </label>
+            <input type="file" id='image' hidden required/>
+          </div>
+          <div className='add-product-name'>
+            <p>Name</p>
+            <input type="text" placeholder= "Type here"/>
+          </div>
+          <div className='add-product-desc'>
+            <p>Name</p>
+            <textarea rows="6" placeholder='write content here'></textarea>
+          </div>
+          <div className='add-category-price'>
+            <div className='add-category'>
+              <p>Category</p>
+              <select >
+                <option value="Rolls">Rolls</option>
+                <option value="Salad">Salad</option>
+                <option value="Dessert">Dessert</option>
+                <option value="Sandwich">Sandwich</option>
+                <option value="Cake">Cake</option>
+                <option value="Pure Veg">Pure Veg</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Noodles">Noodles</option>
+              </select>
+            </div>
+            <div className='add-price'>
+              <p>Price</p>
+              <input type="Number" placeholder='150'/>
+            </div>
+          </div>
+          <button type='submit' className='add-btn'>Add</button>
+        </form>
+      </div>
     </div>
   )
 }

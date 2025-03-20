ad33f5ff69af865e6f81de9644656e180d584a23
Amash Riaz <129083409+Amashriaz1103@users.noreply.github.com>
2025-03-20 20:59:59 +0530

    Add screen styling done

--- adminFrontEnd/src/screens/Add/Add.jsx
+++ adminFrontEnd/src/screens/Add/Add.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import './Add.css'
+import {assets} from '../../assets/assets'
+
 const Add = () => {
   return (
     <div className='screen'>
-      <div className='container'>
-        <form>
-          <div className='add-img-upload'>
+      <div className="container">
+        <form className='flex-col'>
+          <div className="add-img-upload flex-col">
             <p>Upload Image</p>
             <label htmlFor="image">
-              <img src="{assets.upload_area}" alt="" />
+              <img src={assets.upload_area} alt="" />
             </label>
-            <input type="file" id='image' hidden required/>
+            <input type="file" id='image' hidden required />
           </div>
-          <div className='add-product-name'>
+          <div className="add-product-name flex-col">
             <p>Name</p>
-            <input type="text" placeholder= "Type here"/>
+            <input type="text" placeholder='Type here' />
           </div>
-          <div className='add-product-desc'>
-            <p>Name</p>
-            <textarea rows="6" placeholder='write content here'></textarea>
+          <div className="add-product-desc flex-col">
+            <p>Description</p>
+            <textarea rows='6' placeholder='Write content here'></textarea>
           </div>
-          <div className='add-category-price'>
-            <div className='add-category'>
+          <div className="add-category-price">
+            <div className="add-category flex-col">
               <p>Category</p>
-              <select >
-                <option value="Rolls">Rolls</option>
+              <select  >
                 <option value="Salad">Salad</option>
+                <option value="Rolls">Rolls</option>
                 <option value="Dessert">Dessert</option>
                 <option value="Sandwich">Sandwich</option>
                 <option value="Cake">Cake</option>
@@ -34,9 +36,9 @@ const Add = () => {
                 <option value="Noodles">Noodles</option>
               </select>
             </div>
-            <div className='add-price'>
+            <div className="add-price flex-col">
               <p>Price</p>
-              <input type="Number" placeholder='150'/>
+              <input type="Number" placeholder='₹150'/>
             </div>
           </div>
           <button type='submit' className='add-btn'>Add</button>

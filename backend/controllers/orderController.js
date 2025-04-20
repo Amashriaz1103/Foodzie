c6bc71132e9b2861f76684f0a8dc4de21ba893e6
Amash Riaz <129083409+Amashriaz1103@users.noreply.github.com>
2025-04-20 17:55:36 +0530

    update ordercontroller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req,res)=>{
-    const frontend_url = 'http://localhost:5174'
+    // const frontend_url = 'http://localhost:5174'
+    const frontend_url = 'https://foodzieuser.onrender.com'
     try {
         const newOrder = await orderModel.create({
             userId:req.userId,
